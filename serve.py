93be4a417a1ff394d2daae2bb4601e38fb9c0885
Samuel Negash <smlnegash@gmail.com>
2020-08-23 22:10:23 +0300

    Prediction of Sales plot from the dataframe

--- serve.py
+++ serve.py
@@ -4,16 +4,25 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import csv
+from datetime import datetime
+from flask import json
+import pandas as pd
 # import pandas as pd
 # creates a Flask application, named app
 app = Flask(__name__)
 # prediction = pd.read_csv('notebook/data/Rossman_predictions.csv')
 # a route where we will display a welcome message via an HTML template
+data = pd.read_csv('.data/avgpredfile.csv')
+date_column = data['Date'].to_list()
+sales_column = data['Sales'].to_list()
+sales_column = ["{:.2f}".format(x) for x in sales_column]
+date_columns = json.dumps({'date': date_column})
+sales_columns = json.dumps({'sales': sales_column})
 
 
 @app.route("/")
 def home():
-    return render_template('home.html', message="")
+    return render_template('home.html')
 
 
 # a route where we will display a welcome message via an HTML template
@@ -22,25 +31,35 @@ def predict():
     """
     For rendering result on HTML GUI
     """
+    with open('.data/predictionfile.csv') as csv_file:
+        data = csv.DictReader(csv_file, delimiter=',')
+        plot_file = data
+
     if request.method == "GET":
-        return render_template('index.html', select="select date and pridict to see the average sales prediction", date="date_fet", prediction="prediction")
+        message = "Select date and click on pridict button to see the average sales prediction"
+
     elif request.method == "POST":
         date_fet = request.form.get('pred_date')
+        prediction = None
         with open('.data/avgpredfile.csv') as csv_file:
             data = csv.DictReader(csv_file, delimiter=',')
             for row in data:
                 if row['Date'] == date_fet:
                     prediction = row['Sales']
-        return render_template('index.html', select="The Sales Prediction for date: ", date=date_fet, prediction=prediction)
-
+                    prediction = "{:.2f}".format(float(prediction))
+        if(prediction):
+            message = "Sales Prediction for date {} is {}".format(
+                date_fet, prediction)
+        else:
+            message = "Incorrect Date: you have to select date between: 01 Aug, 2015(2015-08-01) and 17 Sep, 2015(2015-09-17)"
+    return render_template('index.html', message=message, labels=date_columns, data=sales_columns)
 
 # a route where we will display a welcome message via an HTML template
 
 
 @app.route("/about")
 def about():
-    message = "sami"
-    return render_template('about.html', message=message)
+    return render_template('about.html')
 
 # a route where we will display a welcome message via an HTML template
 
